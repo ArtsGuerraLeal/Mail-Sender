fff1cfa63eef4ecfb36cc52ccdbbaadc749286c8
Arturo Guerra <guerraarturo11@gmail.com>
2017-08-07 22:32:13 -0500

    Project Upload

---
+++ Mail Sender/Mail Sender/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace Mail_Sender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.txtBody = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.chkRandSub = new System.Windows.Forms.CheckBox();
+            this.chkRandBody = new System.Windows.Forms.CheckBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtInterval = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(38, 260);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Send One";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(87, 18);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(140, 20);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(87, 104);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(140, 20);
+            this.txtTo.TabIndex = 2;
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(87, 130);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(140, 20);
+            this.txtSubject.TabIndex = 3;
+            // 
+            // txtBody
+            // 
+            this.txtBody.Location = new System.Drawing.Point(86, 156);
+            this.txtBody.Name = "txtBody";
+            this.txtBody.Size = new System.Drawing.Size(140, 20);
+            this.txtBody.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(21, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Your Email:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(58, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "To:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(35, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Subject:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Lime;
+            this.label4.Location = new System.Drawing.Point(46, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Body:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // chkRandSub
+            // 
+            this.chkRandSub.AutoSize = true;
+            this.chkRandSub.BackColor = System.Drawing.Color.Lime;
+            this.chkRandSub.Location = new System.Drawing.Point(242, 130);
+            this.chkRandSub.Name = "chkRandSub";
+            this.chkRandSub.Size = new System.Drawing.Size(105, 17);
+            this.chkRandSub.TabIndex = 9;
+            this.chkRandSub.Text = "Random Subject";
+            this.chkRandSub.UseVisualStyleBackColor = false;
+            // 
+            // chkRandBody
+            // 
+            this.chkRandBody.AutoSize = true;
+            this.chkRandBody.BackColor = System.Drawing.Color.Lime;
+            this.chkRandBody.Location = new System.Drawing.Point(242, 158);
+            this.chkRandBody.Name = "chkRandBody";
+            this.chkRandBody.Size = new System.Drawing.Size(93, 17);
+            this.chkRandBody.TabIndex = 10;
+            this.chkRandBody.Text = "Random Body";
+            this.chkRandBody.UseVisualStyleBackColor = false;
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.Lime;
+            this.btnStop.Location = new System.Drawing.Point(260, 260);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 11;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Lime;
+            this.label5.Location = new System.Drawing.Point(25, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Password:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(87, 44);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(140, 20);
+            this.txtPassword.TabIndex = 12;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.Lime;
+            this.btnStart.Location = new System.Drawing.Point(166, 260);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 14;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Lime;
+            this.label6.Location = new System.Drawing.Point(249, 18);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 52);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Remember to\r\nenable less secure \r\napps in your gmail \r\nsettings\r\n";
+            // 
+            // txtInterval
+            // 
+            this.txtInterval.Location = new System.Drawing.Point(243, 201);
+            this.txtInterval.Name = "txtInterval";
+            this.txtInterval.Size = new System.Drawing.Size(92, 20);
+            this.txtInterval.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Lime;
+            this.label7.Location = new System.Drawing.Point(141, 204);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(96, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Auto email interval:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(365, 304);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtInterval);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.chkRandBody);
+            this.Controls.Add(this.chkRandSub);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBody);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Email Sender";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.TextBox txtBody;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox chkRandSub;
+        private System.Windows.Forms.CheckBox chkRandBody;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtInterval;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Mail Sender/Mail Sender/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+
+namespace Mail_Sender
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sendEmail();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sendEmail();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Interval = Convert.ToInt32(txtInterval.Text);
+            timer1.Start();
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+        }
+
+        private void sendEmail()
+        {
+            if (chkRandBody.Checked)
+            {
+                
+                Random rand1 = new Random();
+                int i = rand1.Next(0,100000);
+                txtBody.Text = i.ToString();
+            }
+
+            if (chkRandSub.Checked)
+            {
+                Random rand2 = new Random();
+                int c = rand2.Next(0, 100000);
+                txtSubject.Text = c.ToString();
+            }
+
+
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                Random rand2 = new Random();
+                int c = rand2.Next(0, 100000);
+
+                mail.From = new MailAddress(txtEmail.Text);
+                mail.To.Add(txtTo.Text);
+                mail.Subject = txtSubject.Text + " " + c;
+                mail.Body = txtBody.Text;
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential(txtEmail.Text, txtPassword.Text);
+                SmtpServer.EnableSsl = true;
+               
+                SmtpServer.Send(mail);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+
+
+    
+}
